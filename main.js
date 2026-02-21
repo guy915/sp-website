c6e6abed4bc2cf5c462ca8974a7bae460f8fb1ef
Francisco Antunes Carriço <F.M.AntunesCarrico@student.tudelft.nl>
2026-02-21 20:14:06 +0100

    the cast

--- main.js
+++ main.js
@@ -4,22 +4,34 @@ $.when($.ready).then(() => {
     setTimeout(() => initial.removeClass("initial"), 500);
 });
 
-const bannerImg = $(".banner > img");
+const parallax = $(".parallax");
 
-$(window).on("scroll", () => {
-    const scrollShow = $(".scroll-show");
+function scrollEvent() {
+    const scrollShow = $(".scroll-show:not(.show)");
 
     const scrollTop = $(window).scrollTop();
-    const height = $(window).height();
+    const windowHeight = $(window).height();
 
     scrollShow.each((i, obj) => {
-        const top = $(obj).position().top;
-        console.log(height + scrollTop, top);
+        const top = $(obj).offset().top;
 
-        if (scrollTop + (7 * height) / 8 > top) {
+        if (scrollTop + (7 * windowHeight) / 8 > top) {
             $(obj).addClass("show");
         }
     });
 
-    bannerImg.css("top", scrollTop / 4);
-});
+    parallax.each((i, obj) => {
+        const top = $(obj).offset().top;
+        const height = $(obj).height();
+
+        if (i === 0) console.log(`${i}: ${top}`);
+
+        $(obj)
+            .find("img")
+            .css("height", height + windowHeight / 4)
+            .css("top", (scrollTop - top) / 4);
+    });
+}
+
+$(window).on("scroll", scrollEvent);
+$(window).on("resize", scrollEvent);
