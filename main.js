5fd69537b1e5fc6415dd472ca673cd09d9c5bef4
Francisco Antunes Carriço <F.M.AntunesCarrico@student.tudelft.nl>
2026-02-21 18:58:06 +0100

    animations reeee

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 $.when($.ready).then(() => {
-    const initial = $(".initial")
+    const initial = $(".initial");
 
-    setTimeout(() => initial.removeClass("initial"), 500)
-})
+    setTimeout(() => initial.removeClass("initial"), 500);
+});
+
+$(window).on("scroll", () => {
+    const scrollShow = $(".scroll-show");
+
+    const scrollTop = $(window).scrollTop();
+    const height = $(window).height();
+
+    scrollShow.each((i, obj) => {
+        const top = $(obj).position().top;
+        console.log(height + scrollTop, top);
+
+        if (scrollTop + (7 * height) / 8 > top) {
+            $(obj).addClass("show");
+        }
+    });
+});
